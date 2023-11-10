425a2902a61d19390b10cc9484e71f2525ca4f02
Jason Wu <jasonwu070721@gmail.com>
2023-11-10 15:23:00 +0800

    feat(dataset): add CIFAR100 to train

--- src/MNIST_triplet_loss.py
+++ src/MNIST_triplet_loss.py
@@ -1,4 +1,4 @@
-from torchvision.datasets import MNIST
+from torchvision.datasets import MNIST, CIFAR100
 from torchvision import transforms
 import torch
 from torch.optim import lr_scheduler
@@ -6,21 +6,27 @@ import torch.optim as optim
 from torch.autograd import Variable
 import numpy as np
 
-from datasets import TripletMNIST
+from datasets import TripletMNIST, TripletCIFAR100
 from trainer import fit
 from networks import EmbeddingNet, TripletNet
 from losses import TripletLoss
+from torchsummary import summary
+from torchvision import models
+
 
 def plot_embeddings(embeddings, targets, xlim=None, ylim=None):
-    plt.figure(figsize=(10,10))
+    plt.figure(figsize=(10, 10))
     for i in range(10):
-        inds = np.where(targets==i)[0]
-        plt.scatter(embeddings[inds,0], embeddings[inds,1], alpha=0.5, color=colors[i])
+        inds = np.where(targets == i)[0]
+        plt.scatter(
+            embeddings[inds, 0], embeddings[inds, 1], alpha=0.5, color=colors[i]
+        )
     if xlim:
         plt.xlim(xlim[0], xlim[1])
     if ylim:
         plt.ylim(ylim[0], ylim[1])
     plt.legend(mnist_classes)
+
 
 def extract_embeddings(dataloader, model):
     with torch.no_grad():
@@ -31,40 +37,55 @@ def extract_embeddings(dataloader, model):
         for images, target in dataloader:
             if cuda:
                 images = images.cuda()
-            embeddings[k:k+len(images)] = model.get_embedding(images).data.cpu().numpy()
-            labels[k:k+len(images)] = target.numpy()
+            embeddings[k : k + len(images)] = (
+                model.get_embedding(images).data.cpu().numpy()
+            )
+            labels[k : k + len(images)] = target.numpy()
             k += len(images)
     return embeddings, labels
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print(torch.__version__)
 
     mean, std = 0.1307, 0.3081
 
-    train_dataset = MNIST('../data/MNIST', train=True, download=True,
-                                transform=transforms.Compose([
-                                    transforms.ToTensor(),
-                                    transforms.Normalize((mean,), (std,))
-                                ]))
-    test_dataset = MNIST('../data/MNIST', train=False, download=True,
-                                transform=transforms.Compose([
-                                    transforms.ToTensor(),
-                                    transforms.Normalize((mean,), (std,))
-                                ]))
+    train_dataset = CIFAR100(
+        "../data/CIFAR100",
+        train=True,
+        download=True,
+        transform=transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((mean,), (std,))]
+        ),
+    )
+    test_dataset = CIFAR100(
+        "../data/CIFAR100",
+        train=False,
+        download=True,
+        transform=transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((mean,), (std,))]
+        ),
+    )
     n_classes = 10
 
     cuda = torch.cuda.is_available()
     print(cuda)
 
-    triplet_train_dataset = TripletMNIST(train_dataset) # Returns triplets of images
-    triplet_test_dataset = TripletMNIST(test_dataset)
+    triplet_train_dataset = TripletCIFAR100(train_dataset)  # Returns triplets of images
+    triplet_test_dataset = TripletCIFAR100(test_dataset)
     batch_size = 128
-    kwargs = {'num_workers': 1, 'pin_memory': True} if cuda else {}
-    triplet_train_loader = torch.utils.data.DataLoader(triplet_train_dataset, batch_size=batch_size, shuffle=True, **kwargs)
-    triplet_test_loader = torch.utils.data.DataLoader(triplet_test_dataset, batch_size=batch_size, shuffle=False, **kwargs)
+    kwargs = {"num_workers": 1, "pin_memory": True} if cuda else {}
+    triplet_train_loader = torch.utils.data.DataLoader(
+        triplet_train_dataset, batch_size=batch_size, shuffle=True, **kwargs
+    )
+    triplet_test_loader = torch.utils.data.DataLoader(
+        triplet_test_dataset, batch_size=batch_size, shuffle=False, **kwargs
+    )
 
-    margin = 1.
+    margin = 1.0
     embedding_net = EmbeddingNet()
+    summary(embedding_net.cuda(), (3, 32, 32))
+
     model = TripletNet(embedding_net)
     if cuda:
         model.cuda()
@@ -74,5 +95,15 @@ if __name__ == '__main__':
     scheduler = lr_scheduler.StepLR(optimizer, 8, gamma=0.1, last_epoch=-1)
     n_epochs = 5
     log_interval = 100
-    
-    fit(triplet_train_loader, triplet_test_loader, model, loss_fn, optimizer, scheduler, n_epochs, cuda, log_interval)
+
+    fit(
+        triplet_train_loader,
+        triplet_test_loader,
+        model,
+        loss_fn,
+        optimizer,
+        scheduler,
+        n_epochs,
+        cuda,
+        log_interval,
+    )
--- src/networks.py
+++ src/networks.py
@@ -5,20 +5,39 @@ import torch.nn.functional as F
 class EmbeddingNet(nn.Module):
     def __init__(self):
         super(EmbeddingNet, self).__init__()
-        self.convnet = nn.Sequential(nn.Conv2d(1, 32, 5), nn.PReLU(),
-                                     nn.MaxPool2d(2, stride=2),
-                                     nn.Conv2d(32, 64, 5), nn.PReLU(),
-                                     nn.MaxPool2d(2, stride=2))
 
-        self.fc = nn.Sequential(nn.Linear(64 * 4 * 4, 256),
-                                nn.PReLU(),
-                                nn.Linear(256, 256),
-                                nn.PReLU(),
-                                nn.Linear(256, 2)
-                                )
+        self.conv1 = nn.Conv2d(3, 32, 3, stride=1, padding=1)
+        self.pool1 = nn.MaxPool2d(2, stride=2)
+        self.conv2 = nn.Conv2d(32, 64, 3, stride=1, padding=1)
+        self.pool2 = nn.MaxPool2d(2, stride=2)
+        self.conv3 = nn.Conv2d(64, 128, 3, stride=1, padding=1)
+        self.pool3 = nn.MaxPool2d(2, stride=2)
+        self.conv4 = nn.Conv2d(128, 256, 3, stride=1, padding=1)
+        self.pool4 = nn.MaxPool2d(2, stride=2)
+        self.PReLU = nn.PReLU()
+
+        self.fc = nn.Sequential(
+            nn.Linear(1024, 512),
+            nn.PReLU(),
+            nn.Linear(512, 256),
+            nn.PReLU(),
+            nn.Linear(256, 2),
+        )
 
     def forward(self, x):
-        output = self.convnet(x)
+        output = self.conv1(x)
+        output = self.PReLU(output)
+        output = self.pool1(output)
+        output = self.conv2(output)
+        output = self.PReLU(output)
+        output = self.pool2(output)
+        output = self.conv3(output)
+        output = self.PReLU(output)
+        output = self.pool3(output)
+        output = self.conv4(output)
+        output = self.PReLU(output)
+        output = self.pool4(output)
+
         output = output.view(output.size()[0], -1)
         output = self.fc(output)
         return output
